924443824205839763e53c62c02ba05d61b706e3
Deepu Singla <52960031+AbhinandanSingla@users.noreply.github.com>
2023-01-17 03:33:05 +0530

    added home,products

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import {Navbar} from "./Components/Navbar";
+import {useEffect} from "react";
+import {Home} from "./Components/home";
+import {Products} from "./Components/products";
 
 const App = () => {
     return (<>
         <Navbar/>
+        <Home/>
+        <Products/>
     </>)
 }
 export default App;
