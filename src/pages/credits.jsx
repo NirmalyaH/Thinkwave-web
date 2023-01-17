376d9d55b09a5af1a0920393354cf595ce16e897
Deepu Singla <52960031+AbhinandanSingla@users.noreply.github.com>
2023-01-18 02:18:21 +0530

    add credit page

--- src/pages/credits.jsx
+++ src/pages/credits.jsx
@@ -1,5 +1,7 @@
 import {Navbar} from "../Components/Navbar";
 import "../Components/credits.css";
+import baldeep from "../assets/images/credits/Baldeep.jpeg";
+import {Card} from "react-bootstrap";
 
 export const Credits = () => {
     return (<>
@@ -22,6 +24,25 @@ export const Credits = () => {
                                 alt=""/>
                         </figure>
                     </div>
+                </div>
+                <div className="creditsGalleryContainer">
+                    <Card className={"card"}>
+                        <Card.Img variant="top" src={baldeep} className={"cardImg"}/>
+                        <Card.Body>
+                            <Card.Title className={"cardHeading"}>Baldeep Singh</Card.Title>
+                            <Card.Text className={"cardText"}>
+                                Baldeep Singh currently working as a Software Engineer in Viasat India and graduated
+                                from Thapar Institute of Engineering and Technology (TIET), Patiala, Punjab with B.E. in
+                                Computer Engineering. He got abstract approval of his project i.e. “FarmTech-Realtime
+                                soil quality monitoring system” in international conference of food & security which was
+                                held in TIET (Abstract ID:- ICFS18/0061), Patiala and also got selected for finales of
+                                Startup India Punjab Yatra (ISB Mohali) for the same project.
+                                Area of expertise:-
+                                React js, Python, Arduino uno & Raspberry Pi
+
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
                 </div>
             </div>
         </div>
